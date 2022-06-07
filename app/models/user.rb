2f03e6f59308738d9052c3fbccc654d3cbe8cf13
makeupwith <tarosu1120@gmail.com>
2022-06-07 00:34:36 +0000

    fix by testing model specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,10 +18,11 @@ class User < ApplicationRecord
   before_save { self.email = email.downcase }
   
   # メールアドレス
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 }, 
-    format: { with: VALID_EMAIL_REGEX },
-    uniqueness: { case_sensitive: false }
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # validates :email, presence: true, length: { maximum: 255 }, 
+  #   format: { with: VALID_EMAIL_REGEX },
+  #   uniqueness: { case_sensitive: false }
+  validates :email, presence: true
     
   # パスワード(ハッシュ化)
   has_secure_password # ハッシュ化を行うメソッド
